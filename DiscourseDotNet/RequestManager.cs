abfcf81a897a91bfcddc3ab0594591baf966dc1d
Clifford Duke <clifford.duke@haymarket.asia>
2015-04-20 17:56:31 +0800

    Fixed extension issue

--- DiscourseDotNet/RequestManager.cs
+++ DiscourseDotNet/RequestManager.cs
@@ -3,6 +3,7 @@ using System.Collections.Specialized;
 using System.Net;
 using System.Text;
 using System.Threading.Tasks;
+using DiscourseDotNet.Extensions;
 using DiscourseDotNet.Models;
 using Newtonsoft.Json;
 
@@ -107,17 +108,7 @@ namespace DiscourseDotNet
             return await Task.Factory.StartNew(() => JsonConvert.DeserializeObject<T>(response));
         }
 
-        private static Uri AttachParameters(this Uri uri, NameValueCollection parameters)
-        {
-            var stringBuilder = new StringBuilder();
-            var str = "?";
-            for (var index = 0; index < parameters.Count; ++index)
-            {
-                stringBuilder.Append(str + parameters.AllKeys[index] + "=" + parameters[index]);
-                str = "&";
-            }
-            return new Uri(uri + stringBuilder.ToString());
-        }
+        
 
         private static void HandleException(WebException ex)
         {
